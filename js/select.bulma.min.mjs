79bec57fb76cd30151137741e3e7341c40f453ff
Allan Jardine <allan@datatables.net>
2022-11-04 14:54:48 +0000

    Missing files in DataTables-DataTables

--- js/select.bulma.min.mjs
+++ js/select.bulma.min.mjs
@@ -1,4 +1,1 @@
-/*! Bulma styling wrapper for Select
- * © SpryMedia Ltd - datatables.net/license
- */
 import $ from"jquery";import DataTable from"datatables.net-bm";import DataTable from"datatables.net-select";export default DataTable;
