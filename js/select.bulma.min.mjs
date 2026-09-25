e9acf848409317cc6f61f614676d52bfe39d2219
dtbuild <dtbuild@rigil>
2026-09-25 13:50:56 +0000

    60545c462ae016652eec40667e142a0f2d943a0b Release 4.1.1
    Sync to source repo @60545c462ae016652eec40667e142a0f2d943a0b

--- js/select.bulma.min.mjs
+++ js/select.bulma.min.mjs
@@ -1,4 +1,4 @@
-/*! Select Bulma styling 4.1.0 for DataTables
+/*! Select Bulma styling 4.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bm";import select from"datatables.net-select";export default DataTable;
